c002b0788d6eef1ccc21d18f8c9bec77298c2e75
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-20 11:47:08 +0530

    add ProductCard and AddToCart components

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React, { use } from 'react'
+
+export default function AddToCart() {
+    return (
+        <div>
+            <button onClick={() => console.log('Added to cart')}>Add to Cart</button>
+        </div>
+    )
+}
